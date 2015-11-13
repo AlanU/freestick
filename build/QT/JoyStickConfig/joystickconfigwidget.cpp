79a5a003746f9ab6c66a83149e83423572171e97
AlanU <blackcloak@gmail.com>
2015-11-13 09:40:45 -0600

    changed timer to be longer to keep the cpu down

--- build/QT/JoyStickConfig/joystickconfigwidget.cpp
+++ build/QT/JoyStickConfig/joystickconfigwidget.cpp
@@ -59,7 +59,7 @@ JoyStickConfigWidget::JoyStickConfigWidget(QWidget *parent) :
     timer= new QTimer(this);
     connect(timer,SIGNAL(timeout()),this,SLOT(update()));
 #ifdef Q_OS_WIN32
-    timer->start(30);
+    timer->start(100);
 #else
     timer->start(1000);
 #endif
