434808a83ea84a3f41137fbeca862fec64c81c4c
AlanU <blackcloak@gmail.com>
2016-05-13 00:21:26 -0500

    added common function between xinput and directinput to FSUpdatableJoystickDeviceManager

--- src/USB/common/FSUpdatableJoystickDeviceManager.cpp
+++ src/USB/common/FSUpdatableJoystickDeviceManager.cpp
@@ -10,3 +10,18 @@ void FSUpdatableJoystickDeviceManager::update()
 
 }
 
+void FSUpdatableJoystickDeviceManager::updateEvents(unsigned int joystickDeviceID, FSUSBJoyStickInputElement * elementDevice, PhysicalValueNumber elementValue)
+{
+	if (elementDevice->getValue() != elementValue) {
+
+		if (elementDevice->isIntialized())
+		{
+			FSUSBJoystickDeviceManager::updateEvents(joystickDeviceID, elementDevice, elementValue);
+		}
+		else
+		{
+			std::stack<FSUSBElementInfoMap> inputTypes;
+			elementDevice->getMapping(elementValue, inputTypes);
+		}
+	}
+}
