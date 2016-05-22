b923c783e1d67bf2ad97d9a47bcb2abd8e0637eb
AlanU <blackcloak@gmail.com>
2016-05-22 11:17:49 -0500

    updated Doxygen genreated docs

--- doc/html/search/all_12.js
+++ doc/html/search/all_12.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['safe_5fdelete',['SAFE_DELETE',['../_f_s_direct_input_joystick_manager_8h.html#a9bbcd82e77c41df827c09759def05c9a',1,'FSDirectInputJoystickManager.h']]],
-  ['safe_5frelease',['SAFE_RELEASE',['../_f_s_direct_input_joystick_manager_8h.html#a6038f7bdb274c2e159988a57dedbf93d',1,'FSDirectInputJoystickManager.h']]],
+  ['setcalibrated',['setCalibrated',['../classfreestick_1_1_f_s_direct_input_joystick.html#a57718247e01b1870c2d181d3f6e13bb2',1,'freestick::FSDirectInputJoystick::setCalibrated()'],['../classfreestick_1_1_f_s_x_input_joystick.html#a7b8c4b76daed91e67c09f6cad775f8c0',1,'freestick::FSXInputJoystick::setCalibrated()']]],
+  ['setcalibrationoffsetprecent',['setCalibrationOffsetPrecent',['../classfreestick_1_1_f_s_u_s_b_joy_stick_input_element.html#a491c46dc4bc43894899e11b5e0d82641',1,'freestick::FSUSBJoyStickInputElement']]],
   ['setupiohdimanager',['setupIOHDIManager',['../_f_s_u_s_b_mac_o_s_x_joystick_device_manager_8cpp.html#a9c389b27da91df71189be1b7a6daf743',1,'FSUSBMacOSXJoystickDeviceManager.cpp']]],
   ['setvalue',['setValue',['../classfreestick_1_1_f_s_u_s_b_joy_stick_input_element.html#a42e26aaa09c1e28ad41f7960fd4c3e8a',1,'freestick::FSUSBJoyStickInputElement']]],
   ['sonyvendorid',['SonyVendorID',['../namespacefreestick.html#a95242de1fd151853241cb54a34a99ba2',1,'freestick']]],
