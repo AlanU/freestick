9d28022ffe4576f1513a1bafd3b1a8a3e3ecfbbb
Brian Hapgood <brian.pikaboo@gmail.com>
2020-04-01 11:23:47 -0500

    Fixed up the base class translation units.

--- src/baseClasses/FSBaseManager.cpp
+++ src/baseClasses/FSBaseManager.cpp
@@ -1,29 +1,29 @@
-/**************************************************************************
+/*******************************************************************************
 Created by Alan Uthoff on 10/8/2013
-Copyright (C) 2013.
+Copyright (C) 2013-2020.
 
-This Code is free software; you can redistribute it and/or
-modify it under the terms of the zlib/libpng License as published
-by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-This software is provided 'as-is', without any express or implied warranty.
+This Code is free software; you can redistribute it and/or modify it under the
+terms of the zlib/libpng License as published by the Free Software Foundation;
+either version 2.1 of the License, or (at your option) any later version.  This
+software is provided 'as-is', without any express or implied warranty.
 
-In no event will the authors be held liable for any damages arising from the use of this software.
+In no event will the authors be held liable for any damages arising from the
+use of this software.
 
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute
-it freely, subject to the following restrictions:
+Permission is granted to anyone to use this software for any purpose, including
+commercial applications, and to alter it and redistribute it freely, subject to
+the following restrictions:
 
-1. The origin of this software must not be misrepresented;
-you must not claim that you wrote the original software.
-If you use this software in a product, an acknowledgment
-in the product documentation would be appreciated but is not required.
+1. The origin of this software must not be misrepresented; you must not claim
+that you wrote the original software.  If you use this software in a product,
+an acknowledgment in the product documentation would be appreciated but is not
+required.
 
-2. Altered source versions must be plainly marked as such,
-and must not be misrepresented as being the original software.
+2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original software.
 
 3. This notice may not be removed or altered from any source distribution.
-**************************************************************************/
+*******************************************************************************/
 
 #include "baseClasses/FSBaseManager.h"
 #include <utility>      // std::pair
@@ -31,6 +31,7 @@ and must not be misrepresented as being the original software.
 #include "common/FSDeviceInputEvent.h"
 #include "common/FreeStickLog.h"
 #include <ctime>
+
 using namespace freestick;
 
 FSBaseManager::FSBaseManager()
@@ -38,7 +39,7 @@ FSBaseManager::FSBaseManager()
     _devicIDCreator = nullptr;
 }
 
-void FSBaseManager::init( )
+void FSBaseManager::init()
 {
     //Init joystick here
 
@@ -46,7 +47,7 @@ void FSBaseManager::init( )
 }
 
 
-void FSBaseManager::init(IFSDeviceIDCreator * idCreator)
+void FSBaseManager::init(IFSDeviceIDCreator* idCreator)
 {
     _devicIDCreator = idCreator;
     init();
@@ -55,9 +56,9 @@ void FSBaseManager::init(IFSDeviceIDCreator * idCreator)
 
 FSBaseManager::~FSBaseManager()
 {
-    if( deviceMap.size() !=0)
+    if(deviceMap.size() !=0)
     {
-        for( std::unordered_map<unsigned int, FSBaseDevice * >::iterator itr = deviceMap.begin() ; itr != deviceMap.end(); ++itr)
+        for( auto itr=deviceMap.begin(); itr!=deviceMap.end(); ++itr)
         {
             delete itr->second;
         }
@@ -66,24 +67,24 @@ FSBaseManager::~FSBaseManager()
     _joystickDeviceListeners.clear();
     allJoystickListeners.clear();
     _listenersToCleanUp.clear();
-
 }
 
-const FSBaseDevice * FSBaseManager::getDevice(elementID deviceID)
+const FSBaseDevice* FSBaseManager::getDevice(const elementID deviceID)
 {
     if(deviceMap.find(deviceID) != deviceMap.end())
+    {
         return deviceMap[deviceID];
-    else
-        return nullptr;
+    }
+    return nullptr;
 }
 
-void  FSBaseManager::ListenForAllJoysticksForEventTypes(unsigned int eventFlags,IFSJoystickListener & listener)
+void  FSBaseManager::ListenForAllJoysticksForEventTypes(u32 eventFlags,IFSJoystickListener & listener)
 {
     if(eventFlags & FS_JOYSTICK_CONNECTED_EVENT)
     {
         if(!deviceMap.empty())
         {
-            std::unordered_map<unsigned int, FSBaseDevice * >::iterator itr;
+            std::unordered_map<u32, FSBaseDevice * >::iterator itr;
             for(itr = deviceMap.begin(); itr != deviceMap.end(); ++itr)
             {
                 FSBaseDevice * device = itr->second;
@@ -113,7 +114,7 @@ void  FSBaseManager::ListenForAllJoysticksForEventTypes(unsigned int eventFlags,
     }
 
 }
-void  FSBaseManager::UnListenForAllJoysticksForEventTypes(unsigned int eventFlags,IFSJoystickListener & listener)
+void  FSBaseManager::UnListenForAllJoysticksForEventTypes(u32 eventFlags,IFSJoystickListener & listener)
 {
     if(eventFlags & FS_JOYSTICK_CONNECTED_EVENT)
     {
@@ -340,13 +341,13 @@ void FSBaseManager::removeDevice(FSBaseDevice * device)
 
 }
 
-elementID FSBaseManager::getNextID()
+elementID FSBaseManager::getNextID() const
 {
-    static unsigned int ID = 1;//0 is reserved for errors
+    static u32 ID = 1;//0 is reserved for errors
     if(_devicIDCreator != NULL)
+    {
         return _devicIDCreator->getNextID();
-    else
-        return (ID++);
-
+    }
+    return (ID++);
 }
 
