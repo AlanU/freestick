4e920824f367594ea08b1e9e8cc227893bca1c3f
Brian Hapgood <brian.pikaboo@gmail.com>
2020-04-01 11:19:03 -0500

    Got rid of Sake project; that's a dead tool.

--- build/Sake/gen/ndk/r9d/TestApp/jni/Android.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(LOCAL_PATH)/Android-FreeStick.mk
-include $(LOCAL_PATH)/Android-TestApp.mk
--- build/Sake/gen/ndk/r9d/TestApp/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI      := armeabi armeabi-v7a
-APP_PLATFORM := android-10
-APP_STL      := gnustl_static
-APP_MODULES  := libTestApp
-APP_MODULES  += FreeStick
