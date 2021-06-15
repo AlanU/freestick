2ef1d013cc3eba49654d670cd0526ff901ccc628
Alan Uthoff <blackcloak@gmail.com>
2021-06-14 23:35:32 -0500

    Added Ruby Build File

--- build/QT/CommandLine/build.rb
+++ build/QT/CommandLine/build.rb
@@ -3,9 +3,41 @@ require 'os'
 require 'zip'
 
 BUILD_DIR = 'temp'
+TEST_APP_BUILD_DIR = 'JoyStickConfig'
 OUT_DIR = 'out'
 HEADER_PATH = '../../../inc'
 VISUAL_STUDIO_PATH = 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build'
+QT_DEPLOAY_ARGS="-qmldir=../JoyStickConfig"
+def self.sh (command, command_display_override = nil)
+  result, output = sh_output_result(command, command_display_override)
+  return result
+end
+
+def self.sh_output (command, command_display_override = nil)
+  result, output = self.sh_output_result(command, command_display_override = nil)
+  return output
+end
+
+def self.sh_output_result(command, command_display_override = nil)
+  puts "*** Running ***"
+  display_command = command
+  if (command_display_override != nil)
+    display_command = command_display_override
+  end
+  puts display_command
+  output = `#{command}`
+  result = $?.success?
+  if result
+    puts output if !!ENV['SFVERBOSE'] == true
+    puts "*** Completed #{display_command}"
+  else
+    puts "Failed"
+    puts output
+  end
+  return result, output
+end
+
+
 def build(qtPath, spec , makePath, configString, qmakeProFile)
   # TODO make user there is a way for android to copy java files
   qmake_command = "#{qtPath}/bin/qmake #{qmakeProFile} -r -spec #{spec} #{configString}"
@@ -15,25 +47,48 @@ def build(qtPath, spec , makePath, configString, qmakeProFile)
     qmake_command = "#{vs_command} && " + qmake_command
     make_command = "#{vs_command} && " + make_command
   end
-  qmake_results = system "#{qmake_command}"
-  build_results = system "#{make_command}"
+  qmake_results = sh "#{qmake_command}"
+  build_results = sh "#{make_command}"
   return qmake_results && build_results
 end
 
+def deploy_qt(qt_path, qt_compiler, app_path)
+  if OS.linux?
+    puts "**** Linux Builds Not Supported ****"
+  elsif OS.windows?
+    puts "**** Windows Builds Not Supported ****"
 
-def copyArtifacts(outputName)
+    #windeployqt="#{APP.build_system.path}/#{APP.qt_version}/#{APP.build_system.folder}/bin/windeployqt"
+    # result = BuildUtils.sh("#{windeployqt} \"#{app_path}/Slash N Forge.exe\" #{APP.build_system.deploy_args}")
+    # unless result
+    #   raise "Windeployqt Error"
+    # end
+    #FileUtils.cp_r("#{APP.windows_external_dependencies}/.","#{app_path}")
+    # FileUtils.cp_r("#{APP.windows_external_dependencies}/.","#{app_path}")
+    #FileUtils.cp_r("#{APP.lib_folder}/lexactivator/3.9.0/win32/x64/dll/.","#{app_path}")
+  elsif OS.mac?
+    puts "****** Running Macdeployqt *******"
+    macdeployqt="#{qt_path}/#{qt_compiler}/bin/macdeployqt"
+    result = sh("#{macdeployqt} '#{app_path}' #{QT_DEPLOAY_ARGS}")
+    unless result
+      raise "Macdeployqt Error"
+    end
+  end
+end
+
+def copyArtifacts(outputName, input_dir)
   FileUtils.rm_rf(OUT_DIR)
   Dir.mkdir(OUT_DIR)
   Dir.chdir(OUT_DIR)
   pwd = Dir.pwd
-  FileUtils.cp_r("../#{HEADER_PATH}","../#{BUILD_DIR}/release")
-  #File.rename("./#{BUILD_DIR}", outputName )
-  input_Dir = "../#{BUILD_DIR}/release"
+  #FileUtils.cp_r("../#{HEADER_PATH}", "../#{input_dir}/release")
+  # File.rename("./#{BUILD_DIR}", outputName )
+  input_Dir = "../#{input_dir}/release"
   ::Zip::File.open("#{outputName}.zip", ::Zip::File::CREATE) do |zipFile|
     Dir["#{input_Dir}/**/**"].each do |file|
-      #unless (file == 'release')
+      # unless (file == 'release')
         zipFile.add(file.sub(input_Dir + '/', ''), file)
-      #end
+      # end
     end
   end
 end
@@ -88,20 +143,23 @@ when 'windows'
   spec = 'win32-msvc'
   qt_compiler = 'msvc2017_64'
   config_string = 'CONFIG+=qtquickcompiler'
-  make_path = "C:/Qt/Tools/QtCreator/bin/jom.exe" #TODO use passed in path
+  make_path = "C:/Qt/Tools/QtCreator/bin/jom.exe" # TODO use passed in path
   out_put_file_name = 'windows64'
 end
 
 
-unless spec.empty? && qt_compiler.empty? && config_string.empty?
-  lib_build_results = build("#{$qt_path}/#{qt_compiler}","#{spec}",make_path, config_string,'../../FreeStick/FreeStick.pro')
-  #TODO Fix Building test App
-  #test_app_build_results = build("#{$qt_path}/#{qt_compiler}","#{spec}",make_path, config_string,'../../FreeStickTestApp/FreeStickTestApp.pro')
+if spec.empty? && qt_compiler.empty? && config_string.empty?
+  exit -1
+else
+  lib_build_results = build("#{$qt_path}/#{qt_compiler}", "#{spec}", make_path, config_string, '../../FreeStick/FreeStick.pro')
+  # TODO Fix Building test App
+  test_app_build_results = build("#{$qt_path}/#{qt_compiler}", "#{spec}", make_path, config_string, '../../FreeStickTestApp/FreeStickTestApp.pro')
   Dir.chdir('..')
-  copyArtifacts(out_put_file_name)
+  deploy_qt($qt_path, qt_compiler, './JoyStickConfig/release/JoyStickConfig.app')
+  FileUtils.cp_r("#{HEADER_PATH}", "#{BUILD_DIR}/release") #copy headers over
+  copyArtifacts(out_put_file_name, BUILD_DIR)
+  copyArtifacts('joystickConfig', TEST_APP_BUILD_DIR)
   unless lib_build_results # && test_app_build_results
     exit -1
   end
-else
-  exit -1
 end
